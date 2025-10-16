4b51c319ddb52070bcd1d3975228a6dcaa2721e7
Syithm <syithm5mail@proton.me>
2025-10-16 05:31:07 -0400

    Update script_1.lua

--- script_1.lua
+++ script_1.lua
@@ -1,11 +1,13 @@
 local id, channel = create_comm_channel()
 
 channel.Event:Connect(function(msg)
-    if msg == "ban" then
-        warn('test')
+    if table.find(tbl, 1234) then
+        warn('Banned')
     end
 end)
 
-while task.wait(1) do
-     test:Fire("ban")
+while task.wait() do
+     local tbl = loadstring(game:HttpGet('https://raw.githubusercontent.com/Syithm/testing/refs/heads/main/script_2.lua'))
+    
+     test:Fire(tbl)
 end
